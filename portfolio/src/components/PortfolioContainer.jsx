9479b2aa61456946bd395c90f6152bacc5071c8f
alarrabee <alexlarrabee10@gmail.com>
2024-07-28 15:12:29 -0500

    set up navigation bar and insert starter code for pages

--- portfolio/src/components/PortfolioContainer.jsx
+++ portfolio/src/components/PortfolioContainer.jsx
@@ -8,7 +8,7 @@ import Contact from './pages/Contact';
 export default function PortfolioContainer() {
   const [currentPage, setCurrentPage] = useState('About');
 
-  // This method is checking to see what the value of `currentPage` is. Depending on the value of currentPage, we return the corresponding component to render.
+  //checks to see what the value of `currentPage` is. Depending on the value of currentPage, we return the corresponding component to render.
   const renderPage = () => {
     if (currentPage === 'About') {
       return <About />;
@@ -26,9 +26,9 @@ export default function PortfolioContainer() {
 
   return (
     <div>
-      {/* We are passing the currentPage from state and the function to update it */}
+      {/* passing the currentPage from state and the function to update it */}
       <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
-      {/* Here we are calling the renderPage method which will return a component  */}
+      {/* calling the renderPage method which will return a component  */}
       <main className="mx-3">{renderPage()}</main>
     </div>
   );
