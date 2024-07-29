6d38b9530ba083ba80fcd06308d410acb038b28d
alarrabee <alexlarrabee10@gmail.com>
2024-07-28 20:56:34 -0500

    styles for about page

--- portfolio/src/components/pages/About.jsx
+++ portfolio/src/components/pages/About.jsx
@@ -1,12 +1,45 @@
-import profilePic from '../../assets/profpic.png'
+
+import React from 'react';
+import profilePic from '../../assets/profpic.png';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const styles = {
+    titleStyle: {
+        fontSize: '50px',
+    },
+    container: {
+        display: 'flex', 
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        width: '100%', 
+        padding: '20px',
+        flexDirection: 'row', 
+    },
+    image: {
+        width: '300px', 
+        height: 'auto', 
+        marginRight: '20px', 
+    },
+    text: {
+        marginLeft: '100px',
+        marginRight: '100px', 
+    },
+};
+
 export default function About() {
     return (
-      <div>
-        <h1>About Me</h1>
-        <img src={profilePic} alt="Description of the image" className="about-image" height="200"/>
-        <p>
-        Welcome to my portfolio. I am currently enrolled in the U of M Coding Bootcamp. A 24-week course that will give me the professional skills needed to become a full stack web developer. I have a background in chemistry and am currently in the pharmaceutical and medical device industries. I help build innovative and life saving technology.
-        </p>
-      </div>
+        <div className="container mt-5">
+            <div style={styles.container}>
+                <img 
+                    src={profilePic} 
+                    alt="Description of the image" 
+                    className="img-fluid rounded" 
+                    style={styles.image} 
+                />
+                <p style={styles.text}>
+                    <span style={styles.titleStyle}>About Me:</span><br></br>Welcome to my portfolio. I am currently enrolled in the U of M Coding Bootcamp, a 24-week course that will give me the professional skills needed to become a full stack web developer. I have a background in chemistry and am currently in the pharmaceutical and medical device industries. I help build innovative and life-saving technology.
+                </p>
+            </div>
+        </div>
     );
-  }
+}
