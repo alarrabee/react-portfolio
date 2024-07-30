37ce22f721c20d2c5baee2905b891261b820cdee
alarrabee <alexlarrabee10@gmail.com>
2024-07-30 18:21:41 -0500

    import outlet into App

--- portfolio/src/components/PortfolioContainer.jsx
+++ portfolio/src/components/PortfolioContainer.jsx
@@ -1,39 +1,39 @@
-import { useState } from 'react';
-import NavTabs from './NavTabs';
-import About from './pages/About';
-import Footer from './Footer';
-import Portfolio from './pages/Portfolio';
-import Resume from './pages/Resume';
-import Contact from './pages/Contact';
+// import { useState } from 'react';
+// import NavTabs from './NavTabs';
+// import About from './pages/About';
+// import Footer from './Footer';
+// import Portfolio from './pages/Portfolio';
+// import Resume from './pages/Resume';
+// import Contact from './pages/Contact';
 
 
 
-export default function PortfolioContainer() {
-  const [currentPage, setCurrentPage] = useState('About');
+// export default function PortfolioContainer() {
+//   const [currentPage, setCurrentPage] = useState('About');
 
-  //checks to see what the value of `currentPage` is. Depending on the value of currentPage, we return the corresponding component to render.
-  const renderPage = () => {
-    if (currentPage === 'About') {
-      return <About />;
-    }
-    if (currentPage === 'Portfolio') {
-      return <Portfolio />;
-    }
-    if (currentPage === 'Resume') {
-      return <Resume />;
-    }
-    return <Contact />;
-  };
+//   //checks to see what the value of `currentPage` is. Depending on the value of currentPage, we return the corresponding component to render.
+//   const renderPage = () => {
+//     if (currentPage === 'About') {
+//       return <About />;
+//     }
+//     if (currentPage === 'Portfolio') {
+//       return <Portfolio />;
+//     }
+//     if (currentPage === 'Resume') {
+//       return <Resume />;
+//     }
+//     return <Contact />;
+//   };
 
-  const handlePageChange = (page) => setCurrentPage(page);
+//   const handlePageChange = (page) => setCurrentPage(page);
 
-  return (
-    <div>
-      {/* passing the currentPage from state and the function to update it */}
-      <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
-      {/* calling the renderPage method which will return a component  */}
-      <main className="px-5">{renderPage()}</main>
-      <Footer />
-    </div>
-  );
-}
+//   return (
+//     <div>
+//       {/* passing the currentPage from state and the function to update it */}
+//       <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
+//       {/* calling the renderPage method which will return a component  */}
+//       <main className="px-5">{renderPage()}</main>
+//       <Footer />
+//     </div>
+//   );
+// }
